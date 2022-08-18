d35bc3ab672304351d64afc11428fc6740674477
DrNattapoom <nattapoom.dr@gmail.com>
2022-08-18 14:00:10 +0700

    optimize minimax using aฟแlpha-beta pruning and improve evaluate method

--- ai/minimax.py
+++ ai/minimax.py
@@ -1,74 +1,73 @@
 from copy import deepcopy
 
+from checkers.board import Board
 from checkers.constants import BLACK, WHITE
 
 class Minimax:
 
-    DEFAULT_DEPTH = 3
+    DEFAULT_DEPTH = 5    
 
     def __init__(self, game) -> None:
         self.game = game
-        self.depth = Minimax.DEFAULT_DEPTH
-        self.best_move = None
 
     def move(self, color) -> None:
-        _, new_checkers = self.algorithm(color)
+        _, new_board = self.algorithm(color)
         checkers = self.game.get_checkers()
-        checkers.set_board(new_checkers.get_board())
+        checkers.set_board(new_board)
         checkers.change_turn()
 
     def algorithm(self, to_maximize) -> tuple:
-        def algorithm_helper(checkers, depth, maximize) -> tuple:
-            if (depth == 0 or checkers.get_winner() is not None):
-                return self.evaluate(checkers), checkers
+        def algorithm_helper(board, alpha, beta, depth, maximize) -> tuple:
+            if (depth == 0 or board.get_winner() is not None):
+                return self.evaluate(board), board
             if (maximize):
-                max_score = float("-inf")
-                best_move = None
-                for move in self.get_possible_moves_by_color(checkers, WHITE):
-                    self.depth -= 1
-                    score = algorithm_helper(move, depth - 1, False)[0]
-                    if (score > max_score):
-                        max_score = score
-                        best_move = move
-                return max_score, best_move
+                color = WHITE
+                new_alpha_beta = lambda alpha, beta, score: (max(alpha, score), beta)
+                is_better = lambda new, old: new > old
+                best_score = float("-inf")
             else:
-                min_score = float("inf")
-                best_move = None
-                for move in self.get_possible_moves_by_color(checkers, BLACK):
-                    score = algorithm_helper(move, depth - 1, True)[0]
-                    if (score < min_score):
-                        min_score = score
-                        best_move = move
-                return min_score, best_move
+                color = BLACK
+                new_alpha_beta = lambda alpha, beta, score: (alpha, min(beta, score))
+                is_better = lambda new, old: new < old
+                best_score = float("inf")
+            best_move = None
+            for move in self.get_possible_moves_by_color(board, color):
+                score = algorithm_helper(move, alpha, beta, depth - 1, not maximize)[0]
+                if (is_better(score, best_score)):
+                    best_score = score
+                    best_move = move
+                alpha, beta = new_alpha_beta(alpha, beta, score)
+                if (beta <= alpha):
+                    break
+            return best_score, best_move
         maximize = True if (to_maximize == WHITE) else False
-        return algorithm_helper(self.game.get_checkers(), self.DEFAULT_DEPTH, maximize)
+        return algorithm_helper(self.game.get_checkers().get_board(), float("-inf"), float("inf"), Minimax.DEFAULT_DEPTH, maximize)
 
-    def get_possible_moves_by_color(self, checkers, color):
+    def get_possible_moves_by_color(self, board, color) -> list:
         moves = []
-        for piece in self.get_pieces_by_color(checkers, color):
-            legal_moves = checkers.get_board().find_legal_moves(piece)
+        for piece in self.get_pieces_by_color(board, color):
+            legal_moves = board.find_legal_moves(piece)
             for move, path in legal_moves.items():
-                temp = deepcopy(checkers)
-                new_board = self.simulate(temp, temp.get_piece(piece.row, piece.col), move, path)
+                temp_board = deepcopy(board)
+                temp_piece = temp_board.get_piece(piece.row, piece.col)
+                new_board = self.simulate(temp_board, temp_piece, move, path)
                 moves.append(new_board)
         return moves
 
-    def get_pieces_by_color(self, checkers, color):
-        board = checkers.get_board()
+    def get_pieces_by_color(self, board, color) -> list:
         pieces = sum(board.get_board(), [])
         return list(filter(
             lambda piece: piece is not None and piece.color == color,
             pieces
         ))
-
-    def simulate(self, checkers, piece, move, path):
-        checkers.move(piece, move[0], move[1])
+    
+    def simulate(self, board, piece, move, path) -> Board:
+        board.move(piece, move[0], move[1])
         if (path): 
-            checkers.get_board().remove(path)
-        return checkers
+            board.remove(path)
+        return board
 
-    def evaluate(self, checkers) -> float:
-        board = checkers.get_board()
+    def evaluate(self, board) -> float:        
         black, white = board.get_remaining()
         black_promoted, white_promoted = board.get_promoted()
-        return white - black + (white_promoted * 0.5 - black_promoted * 0.5)
+        return white - black + (white_promoted - black_promoted) * 1.5
